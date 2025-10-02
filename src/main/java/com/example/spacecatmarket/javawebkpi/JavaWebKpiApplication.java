5dacae1e224448848a199186d3a083ca547d48e8
nwu1015 <marchenkov3004@gmail.com>
2025-10-02 17:14:15 +0300

    Initial commit

---
+++ src/main/java/com/example/spacecatmarket/javawebkpi/JavaWebKpiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacecatmarket.javawebkpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebKpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebKpiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spacecatmarket/javawebkpi/JavaWebKpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacecatmarket.javawebkpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebKpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
